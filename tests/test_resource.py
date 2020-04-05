6f21359633b6dedc1345842b9ac5c9e796723248
Gillian Cathomas <gillian.cathomas@gmx.ch>
2020-04-05 13:11:28 +0200

    testing add_weeks_object and added messages to tests

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -9,9 +9,11 @@ class TestResource(common.TransactionCase):
         end_week = 3
         end_year = 2020
         self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year),
-                         [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020}, {'week_num': 3, 'year': 2020}])
+                         [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020}, {'week_num': 3, 'year': 2020}],
+                         'Calculates weeks 1 to 3')
 
-    ##TODO fix method so it works over year bound wrong assert equal here!!!
+        ##TODO fix method so it works over year bound wrong assert equal here!!!
+
     def test_get_week_data_normal_over_year_bound(self):
         start_week = 52
         start_year = 2019
@@ -19,14 +21,15 @@ class TestResource(common.TransactionCase):
         end_year = 2020
         self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year),
                          [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020},
-                          {'week_num': 3, 'year': 2020}])
+                          {'week_num': 3, 'year': 2020}], 'Calculates weeks 52, 2019 to 3, 2020')
 
     def test_get_week_data_start_week_end_week_changed(self):
         start_week = 3
         start_year = 2020
         end_week = 1
         end_year = 2020
-        self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year), [])
+        self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year), [],
+                         'No weeks because start and end date are interchanged')
 
     def test_get_week_data_normal_result_one_week(self):
         start_week = 46
@@ -34,18 +37,21 @@ class TestResource(common.TransactionCase):
         end_week = 46
         end_year = 2020
         self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year),
-                         [{'week_num': 46, 'year': 2020}])
+                         [{'week_num': 46, 'year': 2020}], 'Result is only one week (46)')
 
-    ##TODO fixed that week 0 gets ignored, to be decided if want to keep it that way
+        ##TODO fix that week 0 gets ignored test should fail!! wrong assertion
+
     def test_get_week_data_normal_with_zero(self):
         start_week = 0
         start_year = 2020
         end_week = 2
         end_year = 2020
         self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year),
-                         [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020}])
+                         [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020}],
+                         'Result is week 1 and 2, week 0 gets ignored as starting week')
 
-    ##TODO fixed thath week 0 gets ignored, to be decided if want to keep it that way
+        ##TODO fix that week 0 gets ignored test should fail!! wrong assertion
+
     def test_get_week_data_normal_with_zero_at_end(self):
         start_week = 50
         start_year = 2020
@@ -53,4 +59,21 @@ class TestResource(common.TransactionCase):
         end_year = 2020
         self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year),
                          [{'week_num': 50, 'year': 2020}, {'week_num': 51, 'year': 2020},
-                          {'week_num': 52, 'year': 2020}])
+                          {'week_num': 52, 'year': 2020}],
+                         'Result is week 50, 51 and 52, week 0 gets ignored as ending week')
+
+    def test_add_weeks_object_normal(self):
+        week = {'week_num': 52, 'year': 2020}
+        self.assertEqual(self.env['resource.model'].add_weeks_object(week).week_num, 52, 'Week number is 52')
+        self.assertEqual(self.env['resource.model'].add_weeks_object(week).year, 2020, 'Year is 2020')
+
+    def test_add_weeks_object_old_year(self):
+        week = {'week_num': 50, 'year': 1900}
+        self.assertEqual(self.env['resource.model'].add_weeks_object(week).week_num, 50, 'Week number is 50')
+        self.assertEqual(self.env['resource.model'].add_weeks_object(week).year, 1900, 'Year is 1900')
+
+    def test_add_weeks_object_future_year(self):
+        week = {'week_num': 1, 'year': 3000}
+        self.assertEqual(self.env['resource.model'].add_weeks_object(week).week_num, 1, 'Week number is 1')
+        self.assertEqual(self.env['resource.model'].add_weeks_object(week).year, 3000, 'Year is 3000')
+
