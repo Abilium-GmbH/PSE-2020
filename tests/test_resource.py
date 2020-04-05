c38d16eb4fed5999b51c1e8ffd40356f6e579ce5
Gillian Cathomas <gillian.cathomas@gmx.ch>
2020-04-05 16:22:14 +0200

    added tests for creating resource

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from odoo.tests import common
 
 
@@ -12,8 +14,7 @@ class TestResource(common.TransactionCase):
                          [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020}, {'week_num': 3, 'year': 2020}],
                          'Calculates weeks 1 to 3')
 
-        ##TODO fix method so it works over year bound wrong assert equal here!!!
-
+    ##TODO fix method so it works over year bound wrong assert equal here!!!
     def test_get_week_data_normal_over_year_bound(self):
         start_week = 52
         start_year = 2019
@@ -39,8 +40,7 @@ class TestResource(common.TransactionCase):
         self.assertEqual(self.env['resource.model'].get_week_data(start_week, start_year, end_week, end_year),
                          [{'week_num': 46, 'year': 2020}], 'Result is only one week (46)')
 
-        ##TODO fix that week 0 gets ignored test should fail!! wrong assertion
-
+    ##TODO fix that week 0 gets ignored test should fail!! wrong assertion
     def test_get_week_data_normal_with_zero(self):
         start_week = 0
         start_year = 2020
@@ -50,8 +50,7 @@ class TestResource(common.TransactionCase):
                          [{'week_num': 1, 'year': 2020}, {'week_num': 2, 'year': 2020}],
                          'Result is week 1 and 2, week 0 gets ignored as starting week')
 
-        ##TODO fix that week 0 gets ignored test should fail!! wrong assertion
-
+    ##TODO fix that week 0 gets ignored test should fail!! wrong assertion
     def test_get_week_data_normal_with_zero_at_end(self):
         start_week = 50
         start_year = 2020
@@ -76,4 +75,45 @@ class TestResource(common.TransactionCase):
         week = {'week_num': 1, 'year': 3000}
         self.assertEqual(self.env['resource.model'].add_weeks_object(week).week_num, 1, 'Week number is 1')
         self.assertEqual(self.env['resource.model'].add_weeks_object(week).year, 3000, 'Year is 3000')
+
+    def test_add_weeks_object_normal_2(self):
+        week = {'week_num': 52, 'year': 2050}
+        self.assertNotEqual(self.env['resource.model'].add_weeks_object(week).week_num, 1, 'Week number is not 1')
+        self.assertNotEqual(self.env['resource.model'].add_weeks_object(week).year, 2020, 'Year is not 2020')
+
+    def test_create_resource_normal(self):
+        values = {'project': 2, 'employee': 15, 'workload': 60, 'start_date': '2020-04-05 13:42:07',
+                  'end_date': '2020-04-12 13:42:07'}
+        manual_start_date = datetime(2020, 4, 5, 13, 42, 7)
+        manual_end_date = datetime(2020, 4, 12, 13, 42, 7)
+        created_resource = self.env['resource.model'].create(values)
+        self.assertEqual(created_resource.workload, 60, 'workload is 60%')
+        self.assertEqual(created_resource.start_date, manual_start_date, 'Starting dates is 2020-04-05 13:42:07')
+        self.assertEqual(created_resource.end_date, manual_end_date, 'Ending dates is 2020-04-12 13:42:07')
+        self.assertEqual(int(created_resource.employee.id), 15, 'Id of the assigned employee is 15')
+        self.assertEqual(int(created_resource.project.id), 2, 'Id of the project is 2')
+
+    def test_create_resource_normal_2(self):
+        values = {'project': 3, 'employee': 18, 'workload': 90, 'start_date': '2020-04-13 13:59:40',
+                  'end_date': '2020-05-22 13:59:40'}
+        manual_start_date = datetime(2020, 4, 13, 13, 59, 40)
+        manual_end_date = datetime(2020, 5, 22, 13, 59, 40)
+        created_resource = self.env['resource.model'].create(values)
+        self.assertEqual(created_resource.workload, 90, 'workload is 90%')
+        self.assertEqual(created_resource.start_date, manual_start_date, 'Starting dates is 2020-04-13 13:59:40')
+        self.assertEqual(created_resource.end_date, manual_end_date, 'Ending dates is 2020-05-22 13:59:40')
+        self.assertEqual(int(created_resource.employee.id), 18, 'Id of the assigned employee is 18')
+        self.assertEqual(int(created_resource.project.id), 3, 'Id of the project is 3')
+
+    def test_create_resource_normal_3(self):
+        values = {'project': 6, 'employee': 6, 'workload': 100, 'start_date': '2020-04-19 14:12:04',
+                  'end_date': '2020-05-03 14:12:04'}
+        manual_start_date = datetime(2020, 4, 19, 14, 12, 3)
+        manual_end_date = datetime(2020, 5, 3, 14, 12, 5)
+        created_resource = self.env['resource.model'].create(values)
+        self.assertNotEqual(created_resource.workload, 90, 'workload is not 90%')
+        self.assertNotEqual(created_resource.start_date, manual_start_date, 'Starting dates is not 2020-04-19 14:12:04')
+        self.assertNotEqual(created_resource.end_date, manual_end_date, 'Ending dates is not 2020-05-03 14:12:04')
+        self.assertNotEqual(int(created_resource.employee.id), 5, 'Id of the assigned employee is 5')
+        self.assertNotEqual(int(created_resource.project.id), 3, 'Id of the project is not 3')
 
