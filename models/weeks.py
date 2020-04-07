8d6d072ef8e95689b94307b62d2e95660a3994b3
pfisteso <sophie.pfister@students.unibe.ch>
2020-04-07 09:46:35 +0200

    Extended weeks.py Stores a formatted string-representation (week_string) of each week model now. Integrated week_string in weekly_resource.xml view

--- models/weeks.py
+++ models/weeks.py
@@ -6,6 +6,14 @@ class Weeks(models.Model):
     _name = "week.model"
     week_num = fields.Integer(string='Week', required=True)
     year = fields.Integer(string="Year", required=True)
+    week_string = fields.Char(string="Week String", compute='get_week_string', store=True)
+
+    @api.depends('year', 'week_num')
+    def get_week_string(self):
+        for s in self:
+            string = str(s.year) + ', W' + str(s.week_num)
+            s.week_string = string
+        return 0
 
     @api.constrains('week_num')
     def _check_if_week_num_is_valid(self):
