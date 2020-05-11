33f916593127114607fd903cb3405de3e2feb6e0
pfisteso <sophie.pfister@students.unibe.ch>
2020-05-11 13:32:12 +0200

    Documenting resource_planning_report

--- resource_planning_report/__manifest__.py
+++ resource_planning_report/__manifest__.py
@@ -3,11 +3,14 @@
     'name': "resource_planning_report",
 
     'summary': """
-        Extends resource_planning by adding report-feature
+        Report feature for resource_planning module
     """,
 
     'description': """
-        Provides PDF-Report for resouce_planning
+        This module extends resource_planning module by adding the report function.
+        The assigned resources during a user-selected time can be exported in PDF-Format.
+        The resources are displayed in a clear table similar to the overview in resource_planning module.
+        The total workload per week is calculated and displayed at the bottom of the table.
     """,
 
     'author': "Cyrill Rohrbach, Gillian Cathomas, Sophie Pfister, Joel Hari, Jonas Ph. Kocher",
@@ -16,7 +19,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Tools',
+    'category': 'Administration',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
