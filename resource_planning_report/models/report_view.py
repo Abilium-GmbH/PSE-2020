b295eb585487665e8fa96001c0b06253edfa7b9e
pfisteso <sophie.pfister@students.unibe.ch>
2020-05-11 13:56:11 +0200

    Documenting resource_planning_report

--- resource_planning_report/models/report_view.py
+++ resource_planning_report/models/report_view.py
@@ -6,19 +6,28 @@ from odoo import models, fields, api
 class ReportView(models.AbstractModel):
     """
         Abstract Model for report template.
+        Calculates the data to be passed on to the report.
     """
     _name = 'report.resource_planning_report.planning_report_view'
-    _description='Report View'
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        """
+        :param docids: the doc_id to be concatenated to the file name
+        :param data: passed on from the report_wizard
+        :return: the data for the report
+        """
         weeks = data['form']['weeks']
         length = len(weeks)
+
+        # result
         docs = []
 
+        # Get a list  of all projects and employees
         projects = self.env['project.project'].search([])
         employees = self.env['hr.employee'].search([])
 
+        # Iterate through projects and employees
         for project in projects:
             for employee in employees:
                 weekly_resource = self.env['weekly_resource.model'].search([
@@ -28,30 +37,31 @@ class ReportView(models.AbstractModel):
                 week_array = []
                 for i in range(length):
                     is_set = False
+                    # look for matching weekly_resources
                     for res in weekly_resource:
                         if res.week_string == weeks[i]:
-
                             # check whether there already is an element with this week_string and delete it
                             for week_data in week_array:
                                 if res.week_string == week_data['week']:
                                     week_array.remove(week_data)
+                            # add workload to to the resources week_array
                             week_array.append({'week': weeks[i],
                                                'workload': res.weekly_workload})
                             is_set = True
-                        else:
-                            if not is_set:
-                                week_array.append({'week': weeks[i],
-                                                   'workload': 0})
-                                is_set = True
+                        # else: set up resource with workload 0 and add it to the week_array
+                        elif not is_set:
+                           week_array.append({'week': weeks[i],
+                                               'workload': 0})
+                           is_set = True
 
+                # add data to docs
                 if week_array:
-                    print(employee.name)
-                    print(str(week_array))
                     docs.append({'project': project.name,
                                  'employee': employee.name,
                                  'weekly_data': week_array
                                  })
 
+        # Calculate the total for each week
         week_array = []
         for week in weeks:
             weekly_total = 0
@@ -62,21 +72,15 @@ class ReportView(models.AbstractModel):
             week_array.append({'week': week,
                                'workload': weekly_total
                                })
-
+        # Add total to docs
         docs.append({'project': ' ',
                      'employee': 'Total',
                      'weekly_data': week_array
                      })
-        print('Weeks')
-        print(str(weeks))
 
-        result = {
+        return {
             'doc_ids': data['ids'],
             'doc_model': data['model'],
             'weeks': weeks,
             'docs': docs,
         }
-
-        print(str(result))
-
-        return result
