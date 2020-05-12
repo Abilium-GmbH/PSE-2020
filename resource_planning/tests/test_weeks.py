d06e74d14836c8c6a04eae43d3b0c5989f1b08a6
Gillian Cathomas <gillian.cathomas@gmx.ch>
2020-05-12 17:19:29 +0200

    added tests incl comments

--- resource_planning/tests/test_weeks.py
+++ resource_planning/tests/test_weeks.py
@@ -105,27 +105,6 @@ class TestWeeks(common.TransactionCase):
         self.assertEqual(week.name_get()[0][1], 'Week 2 2021', "Week string should be Week 2")
 
 # -----------------------------------------------------------------------------------------------------------------------
-
-    def test_set_is_week_in_period_week_bool_false(self):
-        """
-                tests if week_bool is set to false with a
-                week_delta of 8
-        """
-        project = self.env['project.project'].create({'name': 'p1'})
-        employee = self.env['hr.employee'].create({'name': 'e1'})
-        values = {'project': project.id,
-                  'employee': employee.id,
-                  'base_workload': 50,
-                  'start_date': '2020-04-13 13:42:07',
-                  'end_date': '2020-04-17 13:42:07'}
-        self.resource = self.env['resource.model'].create(values)
-        today = datetime(2020, 5, 10, 13, 42, 7)
-        this_week = today - timedelta(today.weekday())
-        self.env['week.model'].set_is_week_in_period(this_week,8)
-        if self.env['week.model'].week_num == 14:
-            week = self.env['week.model']
-            self.assertFalse(week.week_bool, "Week_bool is false therefore week is before this week")
-
     def test_set_is_week_in_period_week_bool_true(self):
         """
                     tests if week_bool is set to true with a
@@ -142,10 +121,29 @@ class TestWeeks(common.TransactionCase):
             'year': 2020
         })
 
-        self.env['week.model'].set_is_week_in_period(this_week,8)
-
+        self.env['week.model'].set_is_week_in_period(this_week, 8)
         for week in self.env['week.model']:
-            self.assertEqual(week.week_bool, True, "Week_bool is true therefore week is after this week")
+            self.assertTrue(week.week_bool, "Week_bool is true therefore week is after this week")
+
+    def test_set_is_week_in_period_week_bool_false(self):
+        """
+                tests if week_bool is set to false with a
+                week_delta of 8
+        """
+        today = datetime(2020, 5, 10, 13, 42, 7)
+        this_week = today - timedelta(today.weekday())
+        self.env['week.model'].create({
+            'week_num': 19,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 18,
+            'year': 2020
+        })
+
+        self.env['week.model'].set_is_week_in_period(this_week, 8)
+        if self.env['week.model'].week_num == 14:
+            self.assertFalse(self.env['week.model'].week_bool, "Week_bool is false therefore week is before this week")
 
     def test_set_is_week_in_period_week_bool_mixed(self):
         """
@@ -166,9 +164,9 @@ class TestWeeks(common.TransactionCase):
 
         for week in self.env['week.model']:
             if week.week_num == 18:
-                self.assertEqual(week.week_bool, False, "Week_bool is false therefore week is before this week")
+                self.assertFalse(week.week_bool,"Week_bool is false therefore week is before this week")
             else:
-                self.assertEqual(week.week_bool, True,
+                self.assertTrue(week.week_bool,
                                  "Week_bool is true therefore week is after this week")
 
     def test_set_is_week_in_period_week_bool_false_over_year_bound(self):
@@ -188,7 +186,7 @@ class TestWeeks(common.TransactionCase):
         })
         self.env['week.model'].set_is_week_in_period(this_week,8)
         for week in self.env['week.model']:
-            self.assertEqual(week.week_bool, False, "Week_bool is false therefore week is before this week")
+            self.assertFalse(week.week_bool, "Week_bool is false therefore week is before this week")
 
     def test_set_is_week_in_period_week_bool_true_over_year_bound(self):
         """
@@ -207,5 +205,127 @@ class TestWeeks(common.TransactionCase):
         })
         self.env['week.model'].set_is_week_in_period(this_week,8)
         for week in self.env['week.model']:
-            self.assertEqual(week.week_bool, True, "Week_bool is true therefore week is after this week")
+            self.assertTrue(week.week_bool, "Week_bool is true therefore week is after this week")
+
+    def test_set_is_week_in_period_week_bool_true_going_backwards(self):
+        """
+                tests if week_bool is set to true when week delta is negative
+
+        """
+        today = datetime(2020, 11, 9, 13, 42, 7)
+        this_week = today - timedelta(today.weekday())
+        self.env['week.model'].create({
+            'week_num': 44,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 45,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 46,
+            'year': 2020
+        })
+        self.env['week.model'].set_is_week_in_period(this_week,-2)
+        for week in self.env['week.model']:
+            self.assertTrue(week.week_bool, "Week_bool is true therefore week is before this week, because week delta is negative")
+
+    def test_set_is_week_in_period_week_bool_true_week_delta_zero(self):
+        """
+                 tests if week_bool is set to true when week delta is 0, only the current week should be seen
+
+        """
+        today = datetime(2020, 11, 9, 13, 42, 7)
+        this_week = today - timedelta(today.weekday())
+        self.env['week.model'].create({
+            'week_num': 46,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 47,
+            'year': 2020
+        })
+        self.env['week.model'].set_is_week_in_period(this_week, 0)
+        for week in self.env['week.model']:
+            if week.week_num == 46:
+                self.assertTrue(week.week_bool, "Week_bool is true therefore week is before this week, because"
+                                                        " week delta is 0")
+            else:
+                self.assertFalse(week.week_bool,
+                             "Week_bool is false, week delta is 0 ")
+
+    def test_set_is_week_in_period_week_bool_false_going_backwards(self):
+        """
+                tests if week_bool is set to false when week delta is negative
+        """
+        today = datetime(2020, 11, 30, 13, 42, 7)
+        this_week = today - timedelta(today.weekday())
+        self.env['week.model'].create({
+            'week_num': 44,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 45,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 46,
+            'year': 2020
+        })
+        self.env['week.model'].set_is_week_in_period(this_week,-2)
+        for week in self.env['week.model']:
+            self.assertFalse(week.week_bool, "Week_bool is false therefore week is after this week, weeks are"
+                                                    " even further back")
+
+    def test_set_is_week_in_period_week_bool_mixed_going_backwards(self):
+        """
+                tests if week_bool is set to false for week 46 and true for 44 and 45 when week delta is negative
+        """
+        today = datetime(2020, 11, 3, 13, 42, 7)
+        this_week = today - timedelta(today.weekday())
+        self.env['week.model'].create({
+            'week_num': 44,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 45,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 46,
+            'year': 2020
+        })
+        self.env['week.model'].set_is_week_in_period(this_week, -2)
+        for week in self.env['week.model']:
+            if week.week_num == 46:
+                self.assertFalse(week.week_bool, "Week_bool is false, week is after this week")
+            else:
+                self.assertTrue(week.week_bool,
+                                 "Week_bool is true therefore week is before this week, lies within week delta")
+
+    def test_set_is_week_in_period_mixed_going_backwards_over_year_bound(self):
+        """
+             tests if week_bool is set to false for week 52 and true for 1 and 2 when week delta is negative
+        """
+        today = datetime(2021, 1, 5, 13, 42, 7)
+        this_week = today - timedelta(today.weekday())
+        self.env['week.model'].create({
+            'week_num': 52,
+            'year': 2020
+        })
+        self.env['week.model'].create({
+            'week_num': 1,
+            'year': 2021
+        })
+        self.env['week.model'].create({
+            'week_num': 2,
+            'year': 2021
+        })
+        self.env['week.model'].set_is_week_in_period(this_week, -1)
+        for week in self.env['week.model']:
+            if week.week_num == 52:
+                self.assertFalse(week.week_bool, "Week_bool is false because week is too far in the past")
+            else:
+                self.assertTrue(week.week_bool,
+                                 "Week_bool is true therefore week is before this week, lies within week delta")
 
